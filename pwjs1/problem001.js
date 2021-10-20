f3db1d4d3203292767d3dc57062b81a180a71738
pawelp <pawelpietrakowski22012001@gmail.com>
2021-10-20 20:15:18 +0200

    some refactoring in problem001.js

--- pwjs1/problem001.js
+++ pwjs1/problem001.js
@@ -2,13 +2,16 @@ let roundToFixedDecimal = function(number, decimals) {
     return Math.round(number * Math.pow(10, decimals)) / Math.pow(10, decimals);
 }
 
-let triangleArea = function (a, b, c) {
+let heronEquation = function(a, b, c) {
+    let p = (a + b + c) / 2;
+    return Math.sqrt(p * (p-a) * (p-b) * (p-c));
+}
+
+let triangleArea = function(a, b, c) {
     if (a <= 0 || b <= 0 || c <= 0)
         return -1;
 
-    // Heron equation
-    let p = (a + b + c) / 2;
-    let area = Math.sqrt(p * (p-a) * (p-b) * (p-c));
+    let area = heronEquation(a, b, c);
 
     if (area > 0)
         return roundToFixedDecimal(area, 2);
