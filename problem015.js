186156de54c3df478270da0853cc5e135c429519
Pawel Pietrakowski <pawelpietrakowski22012001@gmail.com>
2021-10-26 18:31:48 +0200

    added some comments

--- problem015.js
+++ problem015.js
@@ -1,3 +1,4 @@
 let getShortMessages = function(messages) {
+    //     copy                             deleting some objects
     return messages.map(obj => obj.message).filter(obj => obj.length <= 50);
 }
