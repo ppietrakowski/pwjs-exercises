3df6f66b696006c4200024a3b0210f383374010b
Pawel Pietrakowski <pawelpietrakowski22012001@gmail.com>
2021-10-26 19:46:11 +0200

    end work on problem 18

--- problem018.js
+++ problem018.js
@@ -1,3 +1,8 @@
 let reduce = function(arr, fn, initial) {   
-    
+    return (function inside(index, current) {
+        if (index >= 0)
+            return fn(inside(index - 1, current), arr[index], index, arr);
+        else // has reached index=-1, beyond array
+            return initial;
+     } )(arr.length - 1, initial);
 }
