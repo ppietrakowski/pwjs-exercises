9587f8654e8555d3416a73b3804ec930a4f63d1c
pawelpietrakowski <pawelpietrakowski22012001@gmail.com>
2022-11-22 11:31:16 +0100

    Replaced recurrent function with iterative method in reduce function

--- pwjs-exercises/problem018.js
+++ pwjs-exercises/problem018.js
@@ -1,8 +1,9 @@
-let reduce = function(arr, fn, initial) {   
-    return (function inside(index, current) {
-        if (index >= 0)
-            return fn(inside(index - 1, current), arr[index], index, arr);
-        else // has reached index=-1, beyond array
-            return initial;
-     } )(arr.length - 1, initial);
+let reduce = function (arr, fn, initial) {
+    let accum = initial
+
+    for (let i = 0; i < arr.length; i++) {
+        accum = fn(accum, arr[i], i, arr)
+    }
+
+    return accum
 }
